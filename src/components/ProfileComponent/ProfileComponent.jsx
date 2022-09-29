b87e56c8417c5f9f897b2054f9a825e6b58f6acd
King Joseph I <110317270+KingJosephI@users.noreply.github.com>
2022-09-29 14:05:01 +0100

    feat: style the profile component

--- src/components/ProfileComponent/ProfileComponent.jsx
+++ src/components/ProfileComponent/ProfileComponent.jsx
@@ -1,10 +1,30 @@
 import React from 'react';
 import './ProfileComponent.scss';
+import profilePic from '../../images/image-jeremy.png';
 
 const ProfileComponent = () => {
   return (
     <div className="profile">
-      <h1>Joseph Madras</h1>
+      <div className="profile__details">
+        <div>
+          <img src={profilePic} alt="Jeremy" className="profile__image" />
+        </div>
+        <div className="profile__info">
+          <span>Report for</span>
+          <h1 className="profile__name">
+            Jeremy
+            <br />
+            Robson
+          </h1>
+        </div>
+      </div>
+      <div className="profile__periodicity">
+        <ul>
+          <li>Daily</li>
+          <li className="selected">Weekly</li>
+          <li>Monthly</li>
+        </ul>
+      </div>
     </div>
   );
 };
